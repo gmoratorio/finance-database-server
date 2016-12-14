3fa20d69dc6b9adca747b1c71233eaaa875bf0db
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2016-12-14 13:43:16 -0700

    started tenant table work in KNEX

--- migrations/20161213101712_property.js
+++ migrations/20161213101712_property.js
@@ -1,13 +1,22 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('property', (property) => {
-        property.increments();
-        property.text('address').notNullable();
-        property.text('city').notNullable();
-        property.text('state').notNullable();
-        property.integer('zip');
-        property.text('unit');
-        property.decimal('rent').notNullable();
-    });
+            property.increments();
+            property.text('address').notNullable();
+            property.text('city').notNullable();
+            property.text('state').notNullable();
+            property.integer('zip');
+            property.text('unit');
+            property.decimal('rent').notNullable();
+        })
+        .then(() => {
+          return knex.schema.createTable('tenant', (tenant)=>{
+            tenant.increments();
+            tenant.text('name').notNullable();
+            tenant.integer('age');
+            tenant.integer('house_id');
+            tenant.foreign('house_id').references('house.id').onDelete(CASCADE);
+          });
+        });
 };
 
 exports.down = function(knex, Promise) {
