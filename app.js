be1e8573abc03e767b0dddfef56fcf2c485071e0
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2016-12-15 09:00:00 -0700

    minor edits

--- app.js
+++ app.js
@@ -14,15 +14,26 @@ app.listen(port, () => {
     console.log(`Up and running on port ${port}!`);
 });
 
-//Get all properties
+//Get all of a specific kind of element
 app.get("/:table", (req, res) => {
     const textEntered = req.params.table;
     const tableType = getTableType(textEntered);
-    if (tableType) {
+    if (tableType === 'payment') {
+        database('property')
+            .innerJoin('tenant', 'property.id', 'tenant.property_id')
+            .innerJoin('payment', 'tenant.id', 'payment.tenant_id')
+            .select()
+            .then((result) => {
+                res.json(result);
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    } else if (tableType) {
         database(tableType)
             .select()
-            .then((tableType) => {
-                res.json(tableType);
+            .then((result) => {
+                res.json(result);
             })
             .catch((err) => {
                 res.send(err);
@@ -33,7 +44,7 @@ app.get("/:table", (req, res) => {
 });
 
 
-//Get a specific property
+//Get a specific element
 app.get("/:table/:id", (req, res) => {
     const textEntered = req.params.table;
     const tableType = getTableType(textEntered);
@@ -44,9 +55,9 @@ app.get("/:table/:id", (req, res) => {
                 .select()
                 .where('id', id)
                 .first()
-                .then((tableType) => {
-                    if (tableType) {
-                        res.json(tableType);
+                .then((result) => {
+                    if (result) {
+                        res.json(result);
                     } else {
                         res.status(404);
                         res.send(`404: ${tableType} with ID "${id}" does not exist.`);
@@ -61,8 +72,138 @@ app.get("/:table/:id", (req, res) => {
     } else {
         sendError(textEntered, res);
     }
+});
+
+//Get all tenants for a specific property
+app.get("/property/:id/tenant", (req, res) => {
+    const id = req.params.id;
+    if (checkInteger(id)) {
+        database('property').innerJoin('tenant', 'property.id', 'tenant.property_id')
+            .select()
+            .where('property.id', id)
+            .then((result) => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    res.status(404);
+                    res.send(`404: ${tableType} with ID "${id}" does not exist.`);
+                }
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    } else {
+        res.send(`"${id}" is not an integer.`)
+    }
+});
 
 
+//Get information on a specific tenant from a specific property
+app.get("/property/:propertyid/tenant/:tenantid", (req, res) => {
+    const propertyId = req.params.propertyid;
+    const tenantId = req.params.tenantid;
+    if (checkInteger(propertyId) && checkInteger(tenantId)) {
+        database('property')
+            .innerJoin('tenant', 'property.id', 'tenant.property_id')
+            .select()
+            .where('property.id', propertyId)
+            .where('tenant.id', tenantId)
+            .first()
+            .then((result) => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    res.status(404);
+                    res.send(`404: ${tableType} with ID "${id}" does not exist.`);
+                }
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    } else {
+        res.send(`"${id}" is not an integer.`)
+    }
+});
+
+//Get information for all payments from a specific tenant
+app.get("/property/:propertyid/tenant/:tenantid/payment", (req, res) => {
+    const propertyId = req.params.propertyid;
+    const tenantId = req.params.tenantid;
+    if (checkInteger(propertyId) && checkInteger(tenantId)) {
+        database('property')
+            .innerJoin('tenant', 'property.id', 'tenant.property_id')
+            .innerJoin('payment', 'tenant.id', 'payment.tenant_id')
+            .select()
+            .where('property.id', propertyId)
+            .where('tenant.id', tenantId)
+            .then((result) => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    res.status(404);
+                    res.send(`404: ${tableType} with ID "${id}" does not exist.`);
+                }
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    } else {
+        res.send(`"${id}" is not an integer.`)
+    }
+});
+
+// Get all payments for a specific Property, regardless of Tenant
+app.get("/property/:propertyid/payment", (req, res) => {
+    const propertyId = req.params.propertyid;
+    if (checkInteger(propertyId)) {
+        database('property')
+            .innerJoin('tenant', 'property.id', 'tenant.property_id')
+            .innerJoin('payment', 'tenant.id', 'payment.tenant_id')
+            .select()
+            .where('property_id', propertyId)
+            .then((result) => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    res.status(404);
+                    res.send(`404: ${tableType} with ID "${id}" does not exist.`);
+                }
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    } else {
+        res.send(`"${id}" is not an integer.`)
+    }
+});
+
+//Get information for a specific payment, from a specific tenant, from a specific property
+app.get("/property/:propertyid/tenant/:tenantid/payment/:paymentid", (req, res) => {
+    const propertyId = req.params.propertyid;
+    const tenantId = req.params.tenantid;
+    const paymentId = req.params.paymentid;
+    if (checkInteger(propertyId) && checkInteger(tenantId)) {
+        database('property')
+            .innerJoin('tenant', 'property.id', 'tenant.property_id')
+            .innerJoin('payment', 'tenant.id', 'payment.tenant_id')
+            .select()
+            .where('property.id', propertyId)
+            .where('tenant.id', tenantId)
+            .where('payment.id', paymentId)
+            .then((result) => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    res.status(404);
+                    res.send(`404: ${tableType} with ID "${id}" does not exist.`);
+                }
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    } else {
+        res.send(`"${id}" is not an integer.`)
+    }
 });
 
 
@@ -234,12 +375,3 @@ function returnPOSTBody(tableType, req) {
     }
     return returnBody;
 }
-
-
-// const textEntered = req.params.table;
-// const tableType = getTableType(textEntered);
-// if (tableType) {
-//
-// } else {
-//     sendError(textEntered, res);
-// }
